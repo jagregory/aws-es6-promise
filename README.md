0cd30f86d5550c05e08ee41ce3370d89ac12d9e0
James Gregory <james@jagregory.com>
2014-12-16 13:46:36 +1100

    Wrap AWS SDK calls in ES6 Promises

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-aws-es6-promise
-===============
+# AWS Promise
 
-AWS SDK ES6 Promise wrapper
+A small wrapper around the AWS SDK to use ES6 Promises instead of callbacks.
+
+## Installation
+
+```
+npm install aws-promise
+```
+
+## Usage
+
+```js
+var AWSPromise = require('aws-es6-promise')
+
+var ec2 = new AWSPromise.EC2()
+
+ec2.describeInstances({ params })
+  .then(function(data) {
+    console.log(data)
+  })
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var AWS = require('aws-sdk'),
+  Promise = require('es6-promise').Promise
+
+function isService(k) {
+  return AWS[k].__super__ === AWS.Service
+}
+
+function publicFunctionNamesOf(obj) {
+  var proto = Object.getPrototypeOf(obj)
+  return Object.keys(proto)
+    .filter(function(fnname) {
+      return typeof proto[fnname] === 'function'
+    })
+}
+
+function cb(resolve, reject) {
+  return function(err, data) {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(data)
+    }
+  }
+}
+
+module.exports = Object.keys(AWS)
+  .filter(isService)
+  .reduce(function(services, k) {
+    services[k] = function(svcParams) {
+      var instance = new AWS[k](svcParams)
+
+      return publicFunctionNamesOf(instance).reduce(function(obj, fnname) {
+        obj[fnname] = function(params) {
+          return new Promise(function(resolve, reject) {
+            instance[fnname](params, cb(resolve, reject))
+          })
+        }
+        return obj
+      }, {})
+    }
+    return services
+  }, {})
